87c20a32fdb19ea4857d77590c3bb0731dbfd1e3
YuliaKurilenko <162790631+YuliaKurilenko@users.noreply.github.com>
2025-01-29 13:03:54 +0300

    Update settings.py

--- Tower Defense Game/Tower Defense Game/settings.py
+++ Tower Defense Game/Tower Defense Game/settings.py
@@ -28,14 +28,14 @@ class Settings:
         self.tower_upgrade_cost = 150
         self.tower_sell_percentage = 0.75
 
-        
+        self.enemy_path = self.change_enemy_path(self.pathway)
 
         self.tower_sprites = {
             'basic': 'assets/towers/basic_tower.png',
             'sniper': 'assets/towers/sniper_tower.png',
-            
+            'money': 'assets/towers/towerDefense_tile203.png',
         }
-       
+        self.enemy_sprite = 'assets/enemies/basic_enemy.png'
         self.bullet_sprite = 'assets/bullets/basic_bullet.png'
         self.background_image = 'assets/backgrounds/game_background.png'
 
@@ -45,10 +45,17 @@ class Settings:
         self.enemy_hit_sound = 'assets/sounds/enemy_hit.wav'
         self.background_music = 'assets/sounds/background_music.mp3'
 
-        
+        self.starting_money = 500
         self.lives = 20
 
         self.tower_positions = [(x * self.grid_size[0] + self.grid_size[0] // 2, y * self.grid_size[1] + self.grid_size[1] // 2)
                                 for x in range(1, self.cols) for y in range(3, self.rows)]
 
-    
+    def change_enemy_path(self, pathway:list)->list:
+        """
+        This function changes the enemy path
+        :param pathway: list of enemy paths
+        :return: random enemy path
+        """
+        ind = random.randint(0, len(pathway) - 1)
+        return self.pathway[ind]
